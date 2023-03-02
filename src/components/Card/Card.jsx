dc8d64e1d2c0ac5a6dff41388f47ec477767c115
david medina <rafaelsanchez97.dm@gmail.com>
2023-03-02 01:01:14 -0300

    agragado cosas a las tarjetas

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -4,11 +4,11 @@ export default function Card(props) {
    const {name, species, gender, image, onClose} = props
    return (
       <DivCard>
-         <button className="card_btn" onClick={onClose} style={{backgroundColor:"green"}}>X</button>
+         <button className="card_btn" onClick={onClose} >X</button>
          <h2>{name}</h2>
          <h2>{species}</h2>
          <h2>{gender}</h2>
-         <img src={image} alt={name} style={{ clipPath: "circle()"}}/>
+         <img src={image} alt={name} style={{ clipPath: "circle()", maxWidth: "80%", maxHeight:"80%"}}/>
          {/* <button onClick={}>X</button>
          <h2></h2>
          <h2></h2>
--- src/components/Card/CardStyles.js
+++ src/components/Card/CardStyles.js
@@ -3,5 +3,8 @@ import styled from 'styled-components';
 export const DivCard = styled.div`
     border-style: groove;
     border-radius: 4%;
-    width: 25%
+    background-color: rgba(255,255,255,0.8);
+    &button {
+        background-color: gold;
+    }
 `;
