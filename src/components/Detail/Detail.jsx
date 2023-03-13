5f2220fd5279fad2cdc7550328efc31730b132aa
david medina <rafaelsanchez97.dm@gmail.com>
2023-03-13 13:48:15 -0300

    terminando project routes

--- src/components/Detail/Detail.jsx
+++ src/components/Detail/Detail.jsx
@@ -1,6 +1,27 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react';
+import axios from 'axios';
+import {useParams} from 'react-router-dom';
+
 
 function Detail() {
+
+  const {id} = useParams();
+
+  const [character, setCharacter] = useState({});
+
+  useEffect(() =>{
+
+    axios(`https://rickandmortyapi.com/api/character/${id}`).then(({ data }) => {
+      if (data.name) {
+         setCharacter(data);
+      } else {
+         window.alert('No hay personajes con ese ID');
+      }
+   }).catch(err => console.log(err);
+   return setCharacter({});
+
+  }, [id])
+
   return (
     <div>Detail
       <h1>hola</h1>
